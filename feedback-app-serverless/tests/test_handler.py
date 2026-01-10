cac65375b0d7483cad0a8e0e6a4a3f737a9f1c9e
Mohammed Anfal <102205132+Mhmdanfal@users.noreply.github.com>
2026-01-10 13:48:36 +0530

    Update test_handler.py

--- feedback-app-serverless/tests/test_handler.py
+++ feedback-app-serverless/tests/test_handler.py
@@ -1,19 +1,12 @@
 import sys
 import json
 
-# Tell Python where the Lambda code is
 sys.path.append("feedback-app-serverless/lambda/feedback_handler")
 
 from app import lambda_handler
 
 
-def test_lambda_handler_success():
-    event = {
-        "body": json.dumps({
-            "name": "Test",
-            "email": "test@test.com",
-            "message": "Hello"
-        })
-    }
-
+def test_missing_fields():
+    event = {"body": "{}"}
     response = lambda_handler(event, None)
+    assert response["statusCode"] == 400
